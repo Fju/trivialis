095f65b7d92b6b7805ae7ac2d7625c22d9ca8e97
Florian Winkler <flowi@outlook.com>
2020-01-29 17:21:40 +0100

    Improved proxy

---
+++ dist/proxy.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once "autoload.php";
+require_once "DB.class.php";
+
+header("Content-Type: text/html; charset=utf-8");
+header("Access-Control-Allow-Methods: GET");
+
+$pages = DB::query("SELECT * FROM pages");
+$templates = array();
+$routes = array();
+foreach ($pages as $page) {
+	$content = $page["content"];
+	if ($content === null) $content = "";
+
+	$templates[$page["name"]] = $content;
+
+	if ($page["route"]) $routes[$page["route"]] = $page["name"];
+}
+
+$fields = DB::query("SELECT * FROM fields");
+$substitutes = array();
+foreach ($fields as $field) {
+	if ($field["content"] === null) continue;
+	$name = $field["name"];
+	$content = $field["content"];
+	$substitutes[$name] = $content;
+}
+
+$loader = new \Twig\Loader\ArrayLoader($templates);
+$twig = new \Twig\Environment($loader);
+
+$method = $_SERVER["REQUEST_METHOD"];
+$route = $_GET["route"];
+if ($method === "GET") {
+	if (isset($routes[$route])) {
+		echo $twig->render($routes[$route], $substitutes);
+	} else {
+		echo "<h1>404</h1>";
+	}
+}
